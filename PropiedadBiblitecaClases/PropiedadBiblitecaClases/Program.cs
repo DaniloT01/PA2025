5ab1f7d6bf1fb00183399ed526f016d8fd711711
DaniloT01 <danolotubon-500@hotmail.com>
2025-04-18 16:38:50 -0500

    Add files via upload

---
+++ PropiedadBiblitecaClases/PropiedadBiblitecaClases/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropiedadBiblitecaClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();  // Crear una nueva persona
+            persona.Nombre = "Juan";         // Usar la propiedad para asignar el nombre
+
+            Console.WriteLine($"Nombre de la persona: {persona.Nombre}");
+        }
+    }
+}
